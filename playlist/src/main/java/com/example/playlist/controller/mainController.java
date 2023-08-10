16e411ee338cbc748bfe03e8329789026c85ed1c
leesemin <leesemin222@gmail.com>
2023-08-10 19:34:26 +0900

    일단 저장

--- playlist/src/main/java/com/example/playlist/controller/mainController.java
+++ playlist/src/main/java/com/example/playlist/controller/mainController.java
@@ -1,6 +1,7 @@
 package com.example.playlist.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,7 +13,9 @@ public class mainController {
   }
   
   @GetMapping(value = "/pages/userInfo")
-  public String management() {
+  public String management(Model Model) {
+    String[] ulList = new String[] {"checkbox","no","name","number","id","deposit","score"};
+    Model.addAttribute("ulData", ulList);
     return "userManagement";
   }
 }
