3fb2b0ae4a82065cfe13a919d95ca3d41cc44694
leesemin <leesemin222@gmail.com>
2023-08-10 21:34:39 +0900

    저장

--- playlist/src/main/java/com/example/playlist/controller/mainController.java
+++ playlist/src/main/java/com/example/playlist/controller/mainController.java
@@ -1,5 +1,9 @@
 package com.example.playlist.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +22,11 @@ public class mainController {
   public String management(Model Model) {
     String[] ulList = new String[] {"checkbox","no","name","number","id","deposit","score"};
     String[] divName = new String[] {"","No","이름","번호","아이디","총 입금액","현스코어"};
+    List<Integer> loop = IntStream.rangeClosed(1, 20).boxed().collect(Collectors.toList());
 
     tagModel tagModel = new tagModel(ulList, divName);
     
+    Model.addAttribute("loop", loop);
     Model.addAttribute("tagModel", tagModel);
     return "userManagement";
   }
