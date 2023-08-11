6c8e225f268a3486d34e157244dd7f3315bfe9bf
leesemin <leesemin222@gmail.com>
2023-08-12 01:38:39 +0900

    엔티티 모델에 따른 인터페이스 레포지토리 만들기

---
+++ playlist/src/main/java/com/example/playlist/repository/Example1Repository.java
@@ -0,0 +1,9 @@
+package com.example.playlist.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.playlist.model.Example1;
+
+public interface Example1Repository extends JpaRepository<Example1, Long>{
+  
+}
