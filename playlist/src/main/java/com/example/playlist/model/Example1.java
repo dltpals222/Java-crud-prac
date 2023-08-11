4f785d86dcd6d5c30b1c8bdc233c8c2f23ca1a75
leesemin <leesemin222@gmail.com>
2023-08-12 01:38:15 +0900

    엔티티 모델

---
+++ playlist/src/main/java/com/example/playlist/model/Example1.java
@@ -0,0 +1,36 @@
+package com.example.playlist.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Example1 {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int no;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private int number;
+
+  @Column(nullable = false)
+  private String id;
+
+  @Column(nullable = false)
+  private Long deposit;
+
+  @Column(nullable = false)
+  private int score;
+
+
+}
