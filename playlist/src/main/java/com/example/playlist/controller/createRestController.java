d0f4c6ee0c59bfd00651b7b35f8d8f03fd494db5
leesemin <leesemin222@gmail.com>
2023-08-17 21:10:36 +0900

    create 부분 input태그 위치 수정

--- playlist/src/main/java/com/example/playlist/controller/createRestController.java
+++ playlist/src/main/java/com/example/playlist/controller/createRestController.java
@@ -9,7 +9,7 @@ import com.example.playlist.model.BaseUser;
 
 @RestController
 @RequestMapping(value = "/api")
-public class mainRestController {
+public class createRestController {
   @PostMapping(value = "/userInfo")
   public String postMethod(@RequestBody BaseUser baseUser) {
     return "OK";
