d8a4dd6599563b6b311bb2ff486e0dd82d858f2d
leesemin <leesemin222@gmail.com>
2023-08-18 09:16:06 +0900

    콘솔로 데이터 들어오는 부분 확인하는 코드 작성 및 null값 에러 처리

--- playlist/src/main/java/com/example/playlist/controller/createRestController.java
+++ playlist/src/main/java/com/example/playlist/controller/createRestController.java
@@ -13,13 +13,35 @@ public class createRestController {
 
   @PostMapping(value = "/userInfo")
   public RedirectView postMethod(BaseUser baseUser) {
+    // NullPointerException 에러처리를 위한 try catch
+    try{
+      int lengthName = baseUser.getName().length;
 
-    // BaseUser 객체의 속성 출력 확인용 코드
-    System.out.println("name: " + baseUser.getName());
-    System.out.println("number: " + baseUser.getNumber());
-    System.out.println("id: " + baseUser.getId());
-    System.out.println("deposit: " + baseUser.getDeposit());
-    System.out.println("score: " + baseUser.getScore());
+    // BaseUser 객체의 속성 출력 확인용 코드 + null값이 아닐 경우 콘솔에 찍어주기
+      for (int i = 0; i < lengthName; i++) {
+        if (baseUser.getName()[i] != null && baseUser.getName()[i] != "") {
+            System.out.println("name: " + baseUser.getName()[i]);
+        }
+        
+        if (baseUser.getNumber()[i] != null) {
+            System.out.println("number: " + baseUser.getNumber()[i]);
+        }
+
+        if (baseUser.getId()[i] != null && baseUser.getId()[i] != "") {
+            System.out.println("id: " + baseUser.getId()[i]);
+        }
+
+        if (baseUser.getDeposit()[i] != null) {
+            System.out.println("deposit: " + baseUser.getDeposit()[i]);
+        }
+
+        if (baseUser.getScore()[i] != null) {
+            System.out.println("score: " + baseUser.getScore()[i]);
+        }
+      }
+    } catch (NullPointerException e) {
+      System.out.println("baseUser의 이름 배열이 없습니다.");
+    }
 
     // 이전페이지로 리디렉션 로직
     RedirectView redirectView = new RedirectView();
