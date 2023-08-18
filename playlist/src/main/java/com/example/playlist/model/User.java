9d77864ce17ba72d645743bb1233ee569fb0136a
leesemin <leesemin222@gmail.com>
2023-08-18 11:14:49 +0900

    배열의 길이가 다를때 나타나는 에러를 잡기 위해 만듦

---
+++ playlist/src/main/java/com/example/playlist/model/User.java
@@ -0,0 +1,10 @@
+package com.example.playlist.model;
+
+// 배열의 길이를 맞추기 위한 클래스 생성
+public class User {
+  public String name;
+  public Integer number;
+  public String id;
+  public Long deposit;
+  public Integer score;
+}
