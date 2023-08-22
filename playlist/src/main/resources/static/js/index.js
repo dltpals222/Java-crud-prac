2daf8b00d94296256e5ccab8eef7e695a012f445
leesemin <leesemin222@gmail.com>
2023-08-22 21:02:12 +0900

    fetch 부분 create 추가

--- playlist/src/main/resources/static/js/index.js
+++ playlist/src/main/resources/static/js/index.js
@@ -3,22 +3,27 @@
  * @param {string} endPoint 엔드포인트 지정
  * @param {string} readPageId 읽어올 페이지의 최상위 id
  * @param {string} insertPageId 읽어온 페이지를 넣을 id
+ * @param {string} contentType 기본값은 text/html 그 이외라면 추가
  */
-function fetchModule(endPoint, readPageId, insertPageId) {
-  fetch(endPoint)
-    .then((Response) => Response.text())
-    .then((result) => {
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(result, "text/html");
-      const infoDIv = doc.getElementById(readPageId);
+async function fetchModule(endPoint, insertPageId, contentType = "text/html") {
+  const response = await fetch(endPoint);
+  const result = await response.text();
 
-      //HTML에 입력시킬 태그
-      document.getElementById(insertPageId).innerHTML = infoDIv.innerHTML;
-    });
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(result, contentType);
+  const infoDiv = doc.body.firstChild;
+  console.log(infoDiv);
+
+  //HTML에 입력시킬 태그
+  document.getElementById(insertPageId).innerHTML = infoDiv.innerHTML;
 }
 
-const umInfo = document.getElementById("um-info");
+document.addEventListener("DOMContentLoaded", () => {
+  const umInfo = document.getElementById("um-info");
 
-umInfo.addEventListener("click", () => {
-  fetchModule("/pages/userInfo", "user-management", "user-info");
+  umInfo.addEventListener("click", async () => {
+    await fetchModule("/pages/userInfo", "user-info");
+    await fetchModule("/pages/create", "create-div");
+    await fetchModule("/pages/update", "update-div");
+  });
 });
