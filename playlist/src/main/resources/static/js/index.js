291fd52a6e6f60cd5cc611f99dcfd2de7abcee8b
leesemin <leesemin222@gmail.com>
2023-08-22 20:09:51 +0900

    fetch 데이터 함수로 변경

--- playlist/src/main/resources/static/js/index.js
+++ playlist/src/main/resources/static/js/index.js
@@ -1,14 +1,24 @@
-const umInfo = document.getElementById("um-info");
-
-umInfo.addEventListener("click", () => {
-  fetch("/pages/userInfo")
+/**
+ *
+ * @param {string} endPoint 엔드포인트 지정
+ * @param {string} readPageId 읽어올 페이지의 최상위 id
+ * @param {string} insertPageId 읽어온 페이지를 넣을 id
+ */
+function fetchModule(endPoint, readPageId, insertPageId) {
+  fetch(endPoint)
     .then((Response) => Response.text())
     .then((result) => {
       const parser = new DOMParser();
       const doc = parser.parseFromString(result, "text/html");
-      const infoDIv = doc.getElementById("user-management");
+      const infoDIv = doc.getElementById(readPageId);
 
       //HTML에 입력시킬 태그
-      document.getElementById("user-info").innerHTML = infoDIv.innerHTML;
+      document.getElementById(insertPageId).innerHTML = infoDIv.innerHTML;
     });
+}
+
+const umInfo = document.getElementById("um-info");
+
+umInfo.addEventListener("click", () => {
+  fetchModule("/pages/userInfo", "user-management", "user-info");
 });
