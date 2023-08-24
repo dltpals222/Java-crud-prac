f749d6a7337c2fbf30d6536f480c4fc5b07bca91
leesemin <leesemin222@gmail.com>
2023-08-24 17:40:41 +0900

    controller 에서 get방식으로 받아오려고 하는 중

--- playlist/src/main/java/com/example/playlist/controller/pageController.java
+++ playlist/src/main/java/com/example/playlist/controller/pageController.java
@@ -1,5 +1,6 @@
 package com.example.playlist.controller;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.playlist.model.ReadUserModel;
@@ -66,7 +68,8 @@ public class pageController {
 
     // 표 만들기 위한 정보
     initTagModel(model);
-    
+
     return view;
   }
+
 }
--- playlist/src/main/resources/static/js/index.js
+++ playlist/src/main/resources/static/js/index.js
@@ -11,18 +11,42 @@ async function fetchModule(endPoint, insertPageId, contentType = "text/html") {
   const parser = new DOMParser();
   const doc = parser.parseFromString(result, contentType);
   const infoDiv = doc.body.firstChild;
-  console.log(infoDiv);
 
   //HTML에 입력시킬 태그
   document.getElementById(insertPageId).innerHTML = infoDiv.innerHTML;
 }
 
+// 불러올 html 파일 관리
 document.addEventListener("DOMContentLoaded", () => {
   const umInfo = document.getElementById("um-info");
 
   umInfo.addEventListener("click", async () => {
+    //* 정보
     await fetchModule("/pages/userInfo", "user-info");
+
+    //* 유저정보 입력
     await fetchModule("/pages/create", "create-div");
+
+    //* 유저정보 수정
     await fetchModule("/pages/update", "update-div");
+
+    // update에 사용할 체크박스 값 가져오기
+    (function ($) {
+      $(document).ready(function () {
+        $("#info-update").click(function (e) {
+          e.preventDefault();
+          console.log("info-update 버튼이 클릭되었습니다.");
+
+          var checkedValues = $("input[type=checkbox]:checked")
+            .map(function () {
+              return this.value;
+            })
+            .get()
+            .join(",");
+
+          // window.location.href = "/pages/update?values=" + checkedValues;
+        });
+      });
+    })(jQuery); // update 끝부분
   });
 });
