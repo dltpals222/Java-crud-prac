5b1b3f18365e9ff5a0a21e2a657ddf98f66ed04e
leesemin <leesemin222@gmail.com>
2023-08-24 18:57:41 +0900

    DAO update부분 정보 불러오기 및 정보 수정 메소드 추가(둘다 테스트 X)

--- playlist/src/main/java/com/example/playlist/repository/BaseUserDAO.java
+++ playlist/src/main/java/com/example/playlist/repository/BaseUserDAO.java
@@ -4,7 +4,9 @@ package com.example.playlist.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.example.playlist.model.User;
@@ -17,7 +19,7 @@ public class BaseUserDAO {
     this.jdbcTemplate = jdbcTemplate;
   }
 
-  // create에서 사용될 메소드
+  //? create에서 사용될 메소드
   public int[] insert(List<User> users) {
     List<Object[]> parameters = new ArrayList<>();
 
@@ -35,6 +37,32 @@ public class BaseUserDAO {
     int[] rowsAffected = jdbcTemplate.batchUpdate(sqlInsert, parameters);
 
     return rowsAffected;
+  }
+
+  //? update에 사용될 체크된 리스트 불러오는 메소드
+  public List<User> findByNoList(List<String> noList){
+    String sqlSelect = "SELECT * FROM exampleCrud WHERE no IN (:noList)";
+    MapSqlParameterSource parameters = new MapSqlParameterSource();
+    parameters.addValue("noList",noList);
+    List<User> foundUsers = jdbcTemplate.query(sqlSelect, new BeanPropertyRowMapper<>(User.class));
+    return foundUsers;
+  }
+
+  //? update 쿼리 및 수정하기 위한 메소드
+  public int updateByNo(String no, User updatedUser){
+    String sqlUpdate = "UPDATE examplecrud SET name = ?, number = ?, id = ?, deposit = ?, score = ? WHERE no = ?";
+    Object[] params = {
+      updatedUser.getName(),
+      updatedUser.getNumber(),
+      updatedUser.getId(),
+      updatedUser.getDeposit(),
+      updatedUser.getScore(),
+      no
+    };
+    
+    int rowsAffected = jdbcTemplate.update(sqlUpdate, params);
+    return rowsAffected;
+    
   }
 
 }
