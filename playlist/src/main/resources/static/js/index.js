0ee0dcb6fbcc47d07086e7c7bb9f6a1e66993a46
leesemin <leesemin222@gmail.com>
2023-08-27 01:10:38 +0900

    수정 혹은 삭제 버튼 클릭시 남아있는 html이 활성화되면서 생기는 버그

--- playlist/src/main/resources/static/js/index.js
+++ playlist/src/main/resources/static/js/index.js
@@ -37,20 +37,26 @@ document.addEventListener("DOMContentLoaded", () => {
     await fetchModule("/pages/create", "create-div");
   });
 
-  //? update에 사용할 체크박스 값 가져오기
   document.body.addEventListener("click", async (e) => {
-    if (e.target.id === "info-update") {
-      e.preventDefault();
-      console.log("info-update 버튼이 클릭되었습니다.");
+    const checkedValues = Array.from(document.querySelectorAll("input[type=checkbox]:checked")).map(
+      (checkbox) => checkbox.value
+    );
 
-      const checkedValues = Array.from(
-        document.querySelectorAll("input[type=checkbox]:checked")
-      ).map((checkbox) => checkbox.value);
-
+    if (e.target.id === "info-update" || e.target.id === "info-delete") {
       if (checkedValues.length === 0) {
-        alert("변경할 항목을 선택해주세요.");
+        alert(
+          e.target.id === "info-update"
+            ? "변경할 항목을 선택해주세요."
+            : "삭제할 항목을 선택해주세요."
+        );
         return;
       }
+    }
+
+    if (e.target.id === "info-update") {
+      e.preventDefault();
+
+      console.log("info-update 버튼이 클릭되었습니다.");
 
       //* 유저정보 수정
       await fetchModule("/pages/update?noList=" + checkedValues.join(","), "update-div");
@@ -59,42 +65,29 @@ document.addEventListener("DOMContentLoaded", () => {
 
       // jQuery open modal
       $("#update-modal").modal("show");
-
-      // 모달창을 닫을때 배경div까지 삭제
+      // 종료 시 백드롭 제거 안되는 코드 제거
       $("#update-modal").on("hidden.bs.modal", function (e) {
         $(".modal-backdrop").remove();
+        $("#update-div").html("");
       });
-    }
-  });
-
-  //? delete
-  document.body.addEventListener("click", async (e) => {
-    if (e.target.id === "info-delete") {
+    } else if (e.target.id === "info-delete") {
       e.preventDefault();
+
       console.log("info-delete 버튼이 클릭되었습니다.");
-
-      const checkedValues = Array.from(
-        document.querySelectorAll("input[type=checkbox]:checked")
-      ).map((checkbox) => checkbox.value);
-
-      if (checkedValues.length === 0) {
-        alert("삭제할 항목을 선택해주세요.");
-        return;
-      }
 
       console.log("checkbox: " + checkedValues);
 
-      //* 유저정보
+      //* 유저정보 삭제
       await fetchModule("/pages/delete?noList=" + checkedValues.join(","), "delete-div");
 
       handleDeleteButtonClick(checkedValues);
 
       // jQuery open modal
       $("#delete-modal").modal("show");
-
-      // 모달창을 닫을때 배경div까지 삭제
+      // 종료 시 백드롭 제거 안되는 코드 제거
       $("#delete-modal").on("hidden.bs.modal", function (e) {
         $(".modal-backdrop").remove();
+        $("#delete-div").html("");
       });
     }
   });
